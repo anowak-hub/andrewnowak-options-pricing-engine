f64aa76b138aba126cfc1eba7895e8a84723dfb6
Andrew Nowak <andrewnowak2106@gmail.com>
2026-05-24 17:48:05 -0500

    Added 'test_black_scholes.py' file in the 'tests' folder. Began backtesting the call_price and put_price functions ensure mathematical accuracy

---
+++ tests/test_black_scholes.py
@@ -0,0 +1,20 @@
+# -- Official Automated Testing File for the Black Scholes Model 
+
+# Import
+import sys 
+import os 
+sys.path.append(os.path.abspath("."))
+
+from src.black_scholes import call_price
+
+# Test Call Price Method
+def test_call_price():
+    result = call_price(100, 100, 1, 0.05, 0.2)
+    assert round(result, 2) == 10.45
+
+test_call_price()
+
+# Test Put Price Method 
+def test_put_price():
+    result = call_price(100,100,1,0.05,0.2)
+    assert round(result, 2) == 5.57
