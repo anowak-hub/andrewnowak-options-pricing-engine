0df52da3a9b452df9e80b14cb15295ca61635bde
Andrew Nowak <andrewnowak2106@gmail.com>
2026-05-29 16:22:32 -0500

    Error Resolution for the test_volatility.py file

--- tests/test_volatility.py
+++ tests/test_volatility.py
@@ -4,7 +4,11 @@
 import sys
 import os
 
-sys.path.append(os.path.abspath(".."))
+project_root = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")
+)
+
+sys.path.append(project_root)
 
 import numpy as np
 import pandas as pd
