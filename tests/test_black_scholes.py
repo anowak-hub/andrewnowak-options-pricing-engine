04765620dd4eefa4dfd21fde075411f647409026
anowak-hub <andrewnowak2106@gmail.com>
2026-07-05 15:39:44 -0500

    Fixed Errors on test_black_scholes.py

--- tests/test_black_scholes.py
+++ tests/test_black_scholes.py
@@ -5,16 +5,17 @@ import sys
 import os 
 sys.path.append(os.path.abspath("."))
 
-from src.black_scholes import call_price
+from src.black_scholes import call_price, put_price
 
 # Test Call Price Method
 def test_call_price():
     result = call_price(100, 100, 1, 0.05, 0.2)
     assert round(result, 2) == 10.45
 
-test_call_price()
-
 # Test Put Price Method 
 def test_put_price():
-    result = call_price(100,100,1,0.05,0.2)
+    result = put_price(100,100,1,0.05,0.2)
     assert round(result, 2) == 5.57
+
+test_call_price()
+test_put_price()
